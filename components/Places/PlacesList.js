7156fab7a756b43e8ae2e4a9cf564f221ee359c5
peter_alfie <alfie1104@gmail.com>
2022-07-08 08:06:01 +0900

    feat(placeslist, placeitem): editing the Favorite Place Items

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,13 +1,35 @@
-import { FlatList, Text } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import PlaceItem from "../PlaceItem";
 
 function PlacesList({ places }) {
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No places added yet - start adding some!
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={places}
       key={(item) => item.id}
-      renderItem={<Text>{places.title}</Text>}
+      renderItem={({ item }) => <PlaceItem place={item} />}
     />
   );
 }
 
 export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
