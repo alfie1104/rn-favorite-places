ba91951fce7e7721e97a72fd478c9ea511ce2ff8
peter_alfie <alfie1104@gmail.com>
2023-02-08 20:49:45 +0900

    feat(imagepicker): create ImagePicker component to take photos

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,3 +1,24 @@
-function ImagePicker() {}
+import { launchCameraAsync } from "expo-image-picker";
+import { Button, View } from "react-native";
+
+function ImagePicker() {
+  const takeImageHandler = async () => {
+    // No permissions request is necessary for launching the image library
+    let image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    console.log(image);
+  };
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+}
 
 export default ImagePicker;
